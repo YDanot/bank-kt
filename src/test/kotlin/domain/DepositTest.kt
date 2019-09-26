a579d1fcc12c9b929aa97768a567ce5088f8b35a
yann.danot.c <yann.danot.c@thalesdigital.io>
2019-09-26 17:29:50 +0200

    test renaming

--- src/test/kotlin/domain/DepositTest.kt
+++ src/test/kotlin/domain/DepositTest.kt
@@ -6,15 +6,12 @@ import utils.*
 class DepositTest {
 
     @Test
-    fun should_deposit_10_euros_on_an_empty_account() {
+    fun deposit_should_be_added_to_account() {
         var account = given_an_account_with("0 EUR")
         account = when_I_deposit("10 EUR").on(account)
         then_balance_of(account).should_be("10 EUR")
-    }
 
-    @Test
-    fun should_deposit_10_euros_on_account_with_balance_10() {
-        var account = given_an_account_with("10 EUR")
+        account = given_an_account_with("10 EUR")
         account = when_I_deposit("10 EUR").on(account)
         then_balance_of(account).should_be("20 EUR")
     }
